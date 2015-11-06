db60cbbb1fff38592884f16e12289008d90693ee
Yury Kirienko <y.kirienko@geoscan.aero>
2015-11-06 18:16:26 +0300

    Least squares for GPS constellation

--- proto/rover_position.py
+++ proto/rover_position.py
@@ -88,7 +88,7 @@ def ecef_to_lat_lon_alt(R):
         if abs(delta) < 1e-10 or i == max_iter-1:
             break
         S, C = Sn, Cn
-    print "i =",i
+    # print "i =",i
     theta = np.math.atan2(R[1],R[0])
     Cc = e1 * Cn
     phi = np.sign(R[2])*np.math.atan2(Sn,Cc)
@@ -115,24 +115,52 @@ def lat_lon_alt_to_ecef_xyz(R):
     Z = sin(f)*(h + b2/radius(f))
     return np.array([X,Y,Z])
 
-def dummy_rover_pos(rho,R):
+
+def least_squares(obs, navs):
     """
-    Naïve approach explained in [1], 2.4 (p. 48):
-    calculate rover's position using measurement of 4 pseudoranges to 4 satellites.
-    :param: rho = (r1,r2,r3,r4) -- pseudoranges to sat-s 1,2,3,4
-    :param: R = (R1,R2,R3,R4) -- coordinates  of sat-s 1,2,3,4 at the moment of measurement
-            Ri = (Xi,Yi,Zi) -- coordinates of i-th satellite in ECEF
-    :return: X = M⁻¹ × Y
+    x = (A^TA)^{-1}A^T l
+    Takes observation
+    :return:
     """
-    a = 6378137.0       # Equatorial Radius [m]
-    b = 6356752.314245  # Polar Radius [m]
-    ear = (a+b)/2  # earth's radius #FIXME
-    print "rho", rho
-    print "R0",R
-    sq = lambda x: np.square(x).sum()
-    Y = np.matrix([rho[i]-sq(R[i])-ear for i in range(4)]).transpose()
-    M = np.matrix([np.append(-2*R[i],1) for i in range(4)])
-    return (M.I*Y).transpose()
+    c = 299792428 # speed of light
+    print "=== Least Squares ==="
+    # from A-matrix
+    # ↳ find all possible satellites N
+    sats = []
+    for i,r in enumerate(obs.PRN_number):
+        if obs.obs_data['C1'][i] and obs.obs_data['P2'][i] and 'G' in r:
+            sats += [r]
+    # ↳ if N >= 4: form matrix
+    if len(sats) > 3:
+        # observed [iono-free] pseudoranges
+        P = np.array([obs.ionofree_pseudorange(s) for s in sats])
+        # print "P =",P
+        # get XYZ-coords of satellites
+        XYZs = np.array([navs[s][0].eph2pos(utc2gpst(o.date)) for s in sats])
+        # print "XYZs =",XYZs
+
+    xyz = np.zeros(4) # initial point
+    for itr in range(20):
+        # geometrical ranges
+        rho = np.array([np.sqrt(sum([(x - xyz[i])**2 for i,x in enumerate(XYZs[j])])) for j in range(len(sats))])
+        # print "rho =",rho
+
+        # from A-matrix
+        A = np.matrix([np.append((xyz[:3] - XYZs[i])/rho[i],1) for i in range(len(sats))])
+        # print "A:\n",A
+        AT = A.transpose()
+        # form l-vector
+        l = np.matrix([P[i] - rho[i] + xyz[3] for i in xrange(len(sats))]).transpose()
+        # form x-vector
+        x_hat = ((AT*A).I * AT * l).flatten().getA()[0]
+        # x_hat[3] /= c
+        print "(x,y,z,cδt) =",x_hat
+        # iterate
+        xyz += x_hat
+        delta = np.sqrt(sum(map(lambda k: k**2,x_hat[:3])+[x_hat[3]/c]))
+        if delta < 10.:
+            break
+    return xyz
 
 if __name__ == "__main__":
 
@@ -147,24 +175,17 @@ if __name__ == "__main__":
 
     # Process Obs file
     observations = parse_rinex(obs_file)
-    o = observations[250]
+    o = observations[240]
     print o.sat_types
-    sats = ['G05', 'G16', 'G18', 'G21']
-    for s in sats:
-        print "C1 from %s at time %s: %f [m]"% (s,str(o.date.time()),o.pseudorange(s,'C1'))
-
-    Rho = np.array([o.pseudorange(s,'C1') for s in sats])
+    # sats = ['G05', 'G16', 'G18', 'G21']
     # for s in sats:
-    #     xyz = navigations[s][0].eph2pos(utc2gpst(o.date))
-    #     print xyz
-    XYZs = np.array([navigations[s][0].eph2pos(utc2gpst(o.date)) for s in sats])
-    coords = (dummy_rover_pos(Rho,XYZs)).tolist()[0]
-    t,f,h = ecef_to_lat_lon_alt(coords[:3])
-    print(t,f,h)
-    print "rover here: %.2f, %.2f, %d " % (t, f, int(h))
+    #     print "C1 from %s at time %s: %f [m]"% (s,str(o.date.time()),o.pseudorange(s,'C1'))
 
-    ecef_to_lat_lon_alt([3695041.,3695041.,3695041.])   # 45,45,0
-    ecef_to_lat_lon_alt([6400000*.707,6400000*.707,1])  # 0.45,0
-    ecef_to_lat_lon_alt([1,1,6400000])
-    ecef_to_lat_lon_alt([1,6400000,1])
-    ecef_to_lat_lon_alt([6400000,1,1])
+
+    print lla_string(ecef_to_lat_lon_alt(least_squares(o,navigations)))
+
+    # ecef_to_lat_lon_alt([3695041.,3695041.,3695041.])   # 45,45,0
+    # ecef_to_lat_lon_alt([6400000*.707,6400000*.707,1])  # 0.45,0
+    # ecef_to_lat_lon_alt([1,1,6400000])
+    # ecef_to_lat_lon_alt([1,6400000,1])
+    # ecef_to_lat_lon_alt([6400000,1,1])

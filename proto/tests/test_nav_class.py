a8035540aafc54a39b3fc2f94b422ef017ac7b81
Yury Kirienko <y.kirienko@geoscan.aero>
2015-12-01 14:30:13 +0300

    Test for GLONASS ephemerides

--- proto/tests/test_nav_class.py
+++ proto/tests/test_nav_class.py
@@ -1,9 +1,19 @@
-from numpy import array
+from numpy import array, sqrt
 from unittest import TestCase
 from numpy.testing import assert_allclose
 from proto.nav_data import *
 from proto.parse_rinex import parse_rinex
 from datetime import timedelta
+
+
+def distance(R1, R2):
+    """
+    Calculates euclidean distance (along the straight line)
+    :param R1: vector in ECEF
+    :param R2: vector in ECEF
+    :return: Euclidean distance between R1 and R2 [in meters]
+    """
+    return sqrt(sum(map(lambda x, y: (x - y) ** 2, R1, R2)))
 
 
 class TestNavGPS(TestCase):
@@ -33,9 +43,9 @@ class TestNavGLO(TestCase):
         nav_file = '../../test_data/test.g'
         navigations = parse_rinex(nav_file)
         self.navs = navigations['R19']
-        for n in self.navs:
-            print n.date
-        print self.navs[1].date - self.navs[0].date
+        # for j, n in enumerate(self.navs):
+        #     print j, n.date
+        # print self.navs[1].date - self.navs[0].date
 
     @staticmethod
     def middle(x):
@@ -44,10 +54,14 @@ class TestNavGLO(TestCase):
         :return: array of length N-1 of the middles between elemes of x
         """
         return map(lambda x: x[0].date +
-                             timedelta(seconds=(x[1].date-x[0].date).total_seconds() / 2),
+                             timedelta(seconds=float((x[1].date - x[0].date).total_seconds()) / 4),
                    zip(x[:-1], x[1:]))
 
     def test_eph2pos(self):
         dt = self.middle(self.navs)
-        print dt
-        print self.navs[0].eph2pos(dt[0]), self.navs[1].eph2pos(dt[0])
+        # print "dt =", dt
+        d1 = self.navs[2].eph2pos(dt[1])
+        d2 = self.navs[1].eph2pos(dt[1])
+        # print "d1 =", d1
+        # print "d2 =", d2
+        assert_allclose(d1, d2, rtol=1e-6)

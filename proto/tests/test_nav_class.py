c9fe50ea2ccc7b2c84eaac31459c749b18d7c1a6
Yury Kirienko <y.kirienko@geoscan.aero>
2016-03-01 16:51:38 +0300

    GTime class if NavGPS.eph2pos()

--- proto/tests/test_nav_class.py
+++ proto/tests/test_nav_class.py
@@ -26,20 +26,22 @@ class TestNavGPS(TestCase):
         nav_file = '../../test_data/epgga2.010'
         navigations = parse_rinex(nav_file)
 
-        self.t = timedelta(seconds=346500)
+        # self.t = timedelta(seconds=346500)
+        self.t = 346500     # seconds from epoch
 
         self.n = navigations['G31'][0]
         self.right_pos = array([11660.379642, 11313.211213, -21326.822815])
 
     def test_eph2pos(self):
         XYZ = self.n.eph2pos(self.n.epoch + self.t) / 1000
-        assert_allclose(XYZ, self.right_pos, rtol=1e-6)
         dist = sqrt(sum(map(lambda x, y: (x - y) ** 2, XYZ, self.right_pos)))
-        assert_allclose(dist, 0., atol=1e-2)
         print "%.2f m <-- distance between exact and calculated positions" % (dist * 1000)
+        assert_allclose(XYZ, self.right_pos, rtol=1e-6)
+        assert_allclose(dist, 0., atol=1e-2)
 
 
 class TestNavGLO(TestCase):
+    # TODO: GTime is not implemented yet in NavGLO!
     def setUp(self):
         # nav_file = '../../test_data/test.g'
         nav_file = '../../test_data/log_000.15g'

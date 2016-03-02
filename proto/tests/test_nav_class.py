1159529fd8106d83148d1dad7bcb91ed25b08261
Yury Kirienko <y.kirienko@geoscan.aero>
2016-03-02 11:03:13 +0300

    Test for leap seconds now in `test_nav_class.py`

--- proto/tests/test_nav_class.py
+++ proto/tests/test_nav_class.py
@@ -3,8 +3,9 @@ from random import choice
 from unittest import TestCase
 from numpy.testing import assert_allclose
 from proto.nav_data import *
+from proto.nav_data import def_leap
 from proto.parse_rinex import parse_rinex
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 def distance(R1, R2):
@@ -38,6 +39,9 @@ class TestNavGPS(TestCase):
         print "%.2f m <-- distance between exact and calculated positions" % (dist * 1000)
         assert_allclose(XYZ, self.right_pos, rtol=1e-6)
         assert_allclose(dist, 0., atol=1e-2)
+
+    def test_utc2gps(self):
+        pass
 
 
 class TestNavGLO(TestCase):
@@ -74,3 +78,25 @@ class TestNavGLO(TestCase):
         print "at", dt[num], " d1 =", d1, self.navs[num].date
         print "at", dt[num], " d2 =", d2, self.navs[num+1].date
         assert_allclose(d1, d2, rtol=1e-6) # FIXME: usually it fails
+
+
+class TestDef_leap(TestCase):
+    def test_def_leap(self):
+        self.assertEqual(def_leap(datetime(1980, 11, 22, 0, 0, 0)), 0)
+        self.assertEqual(def_leap(datetime(1981, 11, 22, 0, 0, 0)), 1)
+        self.assertEqual(def_leap(datetime(1982, 11, 22, 0, 0, 0)), 2)
+        self.assertEqual(def_leap(datetime(1983, 11, 22, 0, 0, 0)), 3)
+        self.assertEqual(def_leap(datetime(1985, 11, 22, 0, 0, 0)), 4)
+        self.assertEqual(def_leap(datetime(1988, 11, 22, 0, 0, 0)), 5)
+        self.assertEqual(def_leap(datetime(1990, 11, 22, 0, 0, 0)), 6)
+        self.assertEqual(def_leap(datetime(1991, 11, 22, 0, 0, 0)), 7)
+        self.assertEqual(def_leap(datetime(1992, 11, 22, 0, 0, 0)), 8)
+        self.assertEqual(def_leap(datetime(1993, 11, 22, 0, 0, 0)), 9)
+        self.assertEqual(def_leap(datetime(1994, 11, 22, 0, 0, 0)), 10)
+        self.assertEqual(def_leap(datetime(1996, 11, 22, 0, 0, 0)), 11)
+        self.assertEqual(def_leap(datetime(1998, 11, 22, 0, 0, 0)), 12)
+        self.assertEqual(def_leap(datetime(1999, 11, 22, 0, 0, 0)), 13)
+        self.assertEqual(def_leap(datetime(2006, 11, 22, 0, 0, 0)), 14)
+        self.assertEqual(def_leap(datetime(2009, 11, 22, 0, 0, 0)), 15)
+        self.assertEqual(def_leap(datetime(2012, 11, 22, 0, 0, 0)), 16)
+        self.assertEqual(def_leap(datetime(2015, 11, 22, 0, 0, 0)), 17)
